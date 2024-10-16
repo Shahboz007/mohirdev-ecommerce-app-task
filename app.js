67b0192ae53f016f12f0345ce5e40b35b9f64877
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-10-16 12:31:52 +0500

    done configuration layout

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const path = require("path");
 const colors = require("colors");
+const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
 
 // Initial template engine
-app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Initial template layout
+app.use(expressLayouts);
+app.set('layout','layout/main')
 
 app.use(express.json());
 app.use(express.urlencoded({ extends: false }));
 
 app.use("/", (req, res) => {
-    return res.render("index");
+    return res.render("index", {title: 'Home page'});
 });
 
 const PORT = process.env.PORT || 3030;
