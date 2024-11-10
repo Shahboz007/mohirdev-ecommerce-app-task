9c0cc3cdd4aa1d28b57d8f614a450e1c4a185d5d
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-11-10 22:27:31 +0500

    added dashboard settings page

--- controllers/dashboard/dashboardHomeController.js
+++ controllers/dashboard/dashboardHomeController.js
@@ -1,6 +1,36 @@
 // Desc      Get Dashboard home page
 // Route     GET /dashboard
 // Access    Protected
+const User = require("../../models/User");
 exports.getDashboardHomePage = async (req, res) => {
-    return res.render('dashboard/dashboardHome', {title: 'Dashboard'});
+    res.render('dashboard/dashboardHome', {title: 'Dashboard'});
+}
+
+exports.getDashboardSettingsPage = async (req, res) => {
+    try {
+        const user = await  User.findById(req.session.user._id);
+        res.render('dashboard/dashboardSettings', {title: 'Settings | Dashboard', userData: user});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.updateDashboardUser = async (req, res) => {
+    const {name, email} = req.body;
+
+    try {
+        const user = await User.findById(req.session.user._id);
+
+        if (name) user.name = name;
+        if (email) user.email = email;
+
+        user.save();
+
+        res.redirect('/dashboard/settings');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.updateDashboardUserPassword = async (req, res) => {
 }
