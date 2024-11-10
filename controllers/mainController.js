bb08697893a4e83ebdc60918949badadb982c564
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-11-10 17:25:21 +0500

    added logout and login btn to mainNav

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,7 +1,7 @@
 exports.getHomePage = async(req, res) => {
-    res.render("index", { title: "Home page" });
+    res.render("index", { title: "Home page", isAuth: Boolean(req.session.user) });
 }
 
 exports.getDetailsPage = async(req, res) => {
-    res.render("details", { title: "Product details" });
+    res.render("details", { title: "Product details", isAuth: Boolean(req.session.user) });
 }
