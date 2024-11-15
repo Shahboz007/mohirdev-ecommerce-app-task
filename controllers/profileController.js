8c6095157a7a4ae6535429081f5ac59f2a7ec1a8
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-11-15 13:38:17 +0500

    done update profile

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,6 +1,8 @@
 const Cart = require("../models/Cart");
+const User = require("../models/User");
 
 exports.getProfilePage = async (req, res) => {
+  const user = await User.findById(req.session.user._id);
   const cart = await Cart.findOne({ user: req.session.user._id })
     .populate("products.product")
     .exec();
@@ -10,8 +12,26 @@ exports.getProfilePage = async (req, res) => {
     title: "Profile",
     isAuth: Boolean(req.session.user),
     cartCount,
+    userData: user,
   });
 };
+
+exports.updateUserData = async(req, res) => {
+  const {name, email} = req.body
+  console.log("🚀 ~ exports.updateUserData=async ~ email:", email)
+
+  try{
+    const user = await User.findById(req.session.user._id);
+    if(name) user.name = name;
+    if(email) user.email = email;
+
+    await user.save();
+
+    res.redirect('/profile');
+  }catch(error){
+    console.log(error)
+  }
+}
 
 exports.getProfileOrdersPage = async (req, res) => {
   const cart = await Cart.findOne({ user: req.session.user._id })
--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -1,9 +1,10 @@
-const { getProfilePage, getProfileOrdersPage } = require("../controllers/profileController");
+const { getProfilePage, getProfileOrdersPage, updateUserData } = require("../controllers/profileController");
 const { protected } = require("../middlewares/auth");
 
 const router = require("express").Router();
 
 router.get("/", protected, getProfilePage);
+router.post("/", protected, updateUserData);
 router.get("/orders", protected, getProfileOrdersPage);
 
 module.exports = router;
