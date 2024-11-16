6f2e5587375ff9ca4d5727ec5906f1828f036efc
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-11-16 09:32:34 +0500

    done dashboard order table

--- controllers/dashboard/dashboardOrderController.js
+++ controllers/dashboard/dashboardOrderController.js
@@ -1,5 +1,14 @@
-exports.getDashboardOrderPage = async(req, res) => {
-  res.render('dashboard/order/index', {
-    title: "Orders | Dashboard"
-  });
-}
+const Order = require("../../models/Order");
+
+exports.getDashboardOrderPage = async (req, res) => {
+  try {
+    const order = await Order.find().populate('user');
+
+    res.render("dashboard/order/index", {
+      title: "Orders | Dashboard",
+      orderData: order,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
