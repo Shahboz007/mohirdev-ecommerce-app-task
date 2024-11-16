06412d202efa1055ee99b8d64da009e76648826e
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-11-16 17:08:36 +0500

    done dashboard home analytics

--- controllers/dashboard/dashboardHomeController.js
+++ controllers/dashboard/dashboardHomeController.js
@@ -1,36 +1,83 @@
 // Desc      Get Dashboard home page
 // Route     GET /dashboard
 // Access    Protected
+const Order = require("../../models/Order");
 const User = require("../../models/User");
 exports.getDashboardHomePage = async (req, res) => {
-    res.render('dashboard/dashboardHome', {title: 'Dashboard'});
-}
+  try {
+    // Users
+    const usersCount = await User.countDocuments();
+    // Orders
+    const ordersCount = await Order.countDocuments({ status: "Pending" });
+    // Income
+    const income = await Order.aggregate([
+      {
+        $match: { status: "Delivered" },
+      },
+      {
+        $group: {
+          _id: null,
+          totalIncome: { $sum: "$totalPrice" },
+        },
+      },
+    ]);
+    const totalIncome = income.length > 0 ? income[0].totalIncome : 0;
+    // Products Sold
+    const sold = await Order.aggregate([
+      {
+        $match: { status: "Delivered" },
+      },
+      {
+        $group: {
+          _id: null,
+          totalIncome: { $sum: "$totalPrice" },
+        },
+      },
+    ]);
+
+    const totalSold = sold.length > 0 ? sold[0].totalIncome : 0;
+
+    res.render("dashboard/dashboardHome", {
+      title: "Dashboard",
+      totalData: {
+        usersCount,
+        ordersCount,
+        totalIncome,
+        totalSold,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 exports.getDashboardSettingsPage = async (req, res) => {
-    try {
-        const user = await  User.findById(req.session.user._id);
-        res.render('dashboard/dashboardSettings', {title: 'Settings | Dashboard', userData: user});
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    const user = await User.findById(req.session.user._id);
+    res.render("dashboard/dashboardSettings", {
+      title: "Settings | Dashboard",
+      userData: user,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 exports.updateDashboardUser = async (req, res) => {
-    const {name, email} = req.body;
+  const { name, email } = req.body;
 
-    try {
-        const user = await User.findById(req.session.user._id);
+  try {
+    const user = await User.findById(req.session.user._id);
 
-        if (name) user.name = name;
-        if (email) user.email = email;
+    if (name) user.name = name;
+    if (email) user.email = email;
 
-        await user.save();
+    await user.save();
 
-        res.redirect('/dashboard/settings');
-    } catch (error) {
-        console.log(error);
-    }
-}
+    res.redirect("/dashboard/settings");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-exports.updateDashboardUserPassword = async (req, res) => {
-}
+exports.updateDashboardUserPassword = async (req, res) => {};
